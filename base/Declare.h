ac80f369c6c7ad62d1aebfdb2a54f3f6870a5585
liusy6 <57052696+liusy6@users.noreply.github.com>
2022-03-19 19:20:32 +0800

    Add files via upload

---
+++ base/Declare.h
@@ -0,0 +1,20 @@
+#ifndef DECLEAR_H
+#define DECLEAR_H
+
+class Acceptor;
+class TcpConnection;
+class Channel;
+class EventLoop;
+class Thread;
+class ThreadPool;
+class EventLoopThread;
+class EVentLoopThreadPool;
+class Epoll;
+class Timestamp;
+class TimerQueue;
+class Timer;
+class Buffer;
+class HttpRequest;
+class HttpResponse;
+
+#endif
---
+++ base/Define.h
@@ -0,0 +1,8 @@
+#ifndef DEFINE_H
+#define DEFINE_H
+
+#define MAX_LISTENFD 5
+#define MAX_EVENT 1024
+#define MAX_FD 65536
+#define TimeoutMS 60000
+#endif
